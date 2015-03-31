064601bf6513e2806d400e99cb3099432b10fc42
beatawu <beata.wojtaszuk@gmail.com>
2015-03-31 21:17:37 +0200

    Środowiska #9

---
+++ app/cache/prod/twig/61/4d/e64dc94a33e81215628756e66c1a0562329d771905292457db90b73d1a55.php
@@ -0,0 +1,45 @@
+<?php
+
+/* TwigBundle:Exception:error.css.twig */
+class __TwigTemplate_614de64dc94a33e81215628756e66c1a0562329d771905292457db90b73d1a55 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "/*
+";
+        // line 2
+        echo twig_escape_filter($this->env, (isset($context["status_code"]) ? $context["status_code"] : null), "html", null, true);
+        echo " ";
+        echo twig_escape_filter($this->env, (isset($context["status_text"]) ? $context["status_text"] : null), "html", null, true);
+        echo "
+
+*/
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:error.css.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
---
+++ app/cache/prod/twig/c2/3a/c75017e35968352dc4e1de64d94fb23a50987588cf66f6d9ffb4a23d409d.php
@@ -0,0 +1,42 @@
+<?php
+
+/* TwigBundle:Exception:exception.js.twig */
+class __TwigTemplate_c23ac75017e35968352dc4e1de64d94fb23a50987588cf66f6d9ffb4a23d409d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "/*
+";
+        // line 2
+        $this->env->loadTemplate("TwigBundle:Exception:exception.txt.twig")->display(array_merge($context, array("exception" => (isset($context["exception"]) ? $context["exception"] : null))));
+        // line 3
+        echo "*/
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:exception.js.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  24 => 3,  22 => 2,  19 => 1,);
+    }
+}
